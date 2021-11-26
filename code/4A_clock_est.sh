bd8e6d244f8ac6f298d3ad0b14378e32fa49604d
miraSantos <miras@mit.edu>
2021-11-26 10:37:41 -0500

    added scripts

--- code/4A_clock_est.sh
+++ code/4A_clock_est.sh
@@ -6,20 +6,18 @@
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --mem=15gb                   # Job memory request
 #SBATCH --time=02:00:00             # Time limit hrs:min:sec
-#SBATCH --output=log_atr_%j.log  # Standard output/error
+#SBATCH --output=log_clock_%j.log  # Standard output/error
  
 pwd; hostname; date
  
 module load anaconda3/2021.05       # load anaconda
 
 source activate treetime
-treetime ancestral \
-	--tree /vortexfs1/omics/env-bio/collaboration/env_bio_covid/data/mafft_out/aligned_mafft_ncov.fasta.uniqueseq.phy \
-	--aln /vortexfs1/omics/env-bio/collaboration/env_bio_covid/data/mafft_out/aligned_mafft_ncov.fasta \
-	--gtr JC69 \
-	--keep-overhangs \
-	--outdir /vortexfs1/omics/env-bio/collaboration/env_bio_covid/results/treetime_out/
 
-echo "finished atr" 
+
+
+
+echo "finished clock lengths" 
+
 date
 
